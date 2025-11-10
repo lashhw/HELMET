a8cc3cd914b1aab3d3203e71b6c73c52063928d7
Yen-Chieh Huang <lashhw81@gmail.com>
2025-11-10 20:00:38 +0800

    add `run_all.sh` and `run_vanilla_all.sh`

---
+++ run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+datasets="nq triviaqa hotpotqa popqa rerank narrativeqa infbenchqa infbenchmc infbenchsum lexsum trecc trecf banking clinc nlu"
+lambdas="0.02 0.028 0.04 0.057 0.08 0.16 0.32 0.64 1.28 2.56"
+thresholds="0.01"
+
+for dataset in $datasets; do
+  for lambda in $lambdas; do
+    for threshold in $thresholds; do
+      python eval.py \
+        --config configs/extra/${dataset}_32k.yaml \
+        --seed 42 \
+        --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+        --output_dir output/all/${dataset}_32k/${lambda}-${threshold} \
+        --use_filtering \
+        --filtering_path weights/llama-3.2-1b-${lambda}.pt \
+        --g_threshold ${threshold} \
+        --max_tokens_per_head 65536
+    done
+  done
+done
---
+++ run_vanilla_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+datasets="nq triviaqa hotpotqa popqa rerank narrativeqa infbenchqa infbenchmc infbenchsum lexsum trecc trecf banking clinc nlu"
+
+for dataset in $datasets; do
+  python eval.py \
+    --config configs/extra/${dataset}_32k.yaml \
+    --seed 42 \
+    --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+    --output_dir output/all/${dataset}_32k/vanilla
+done
