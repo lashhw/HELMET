b8cab896ca5e523f43244f7f383a03a1d87c8c7b
Yen-Chieh Huang <lashhw81@gmail.com>
2025-11-17 19:52:37 +0800

    change threshold to 0.1

--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,7 @@ set -e
 
 datasets="nq triviaqa hotpotqa popqa rerank narrativeqa infbenchqa infbenchmc infbenchsum lexsum trecc trecf banking clinc nlu"
 lambdas="0.02 0.028 0.04 0.057 0.08 0.16 0.32 0.64 1.28 2.56"
-thresholds="0.01"
+thresholds="0.1"
 
 for dataset in $datasets; do
   for lambda in $lambdas; do
