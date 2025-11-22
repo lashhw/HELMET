36178186324bf955cac0b027cb79547f9ec74620
Yen-Chieh Huang <lashhw81@gmail.com>
2025-11-22 12:12:04 +0800

    add lambda 0.01

--- run_filtering_all.sh
+++ run_filtering_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 datasets="nq triviaqa popqa hotpotqa rerank asqa qampari narrativeqa infbenchqa infbenchmc infbenchsum lexsum trecc trecf nlu banking clinc"
-lambdas="0.02 0.04 0.08 0.16 0.32 0.64 1.28 2.56"
+lambdas="0.01 0.02 0.04 0.08 0.16 0.32 0.64 1.28 2.56"
 thresholds="0.1"
 
 for dataset in $datasets; do
