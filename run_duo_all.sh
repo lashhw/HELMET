82a64bd0723bfd011bf7b23419e53dbf8297deed
Yen-Chieh Huang <lashhw81@gmail.com>
2025-12-17 20:28:29 +0800

    0.95 sparsity

--- run_duo_all.sh
+++ run_duo_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 datasets="nq triviaqa popqa hotpotqa rerank narrativeqa infbenchqa infbenchmc infbenchsum lexsum trecf nlu banking clinc"
-sparsitys="0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1"
+sparsitys="0.95 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1"
 
 for dataset in $datasets; do
   for sparsity in $sparsitys; do
